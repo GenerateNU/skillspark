GenerateNU/specialstandard

backend + db init + docs

backend/cmd/main.go
@@ -1,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"specialstandard/internal/config"
+	"specialstandard/internal/service"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	"github.com/sethvargo/go-envconfig"
+)
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
+
+	var config config.Config
+	if err := envconfig.Process(context.Background(), &config); err != nil {
+		log.Fatalln("Error processing .env file: ", err)
+	}
+
+	app := service.InitApp(config)
+
+	// Pushing the closing of the database connection onto a
+	// stack of statements to be executed when this function returns.
+
+	// **Uncomment after repo connection is actually made**
+	// defer app.Repo.Close()
+
+	port := config.Application.Port
+	// Listen for connections with a goroutine.
+	go func() {
+		if err := app.Server.Listen(":" + port); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for the termination signal:
+	<-quit
+
+	// Then shutdown server gracefully.
+	slog.Info("Shutting down server")
+	if err := app.Server.Shutdown(); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	}
+
+	slog.Info("Server shutdown")
+}

backend/go.mod
@@ -1,0 +1,34 @@
+module specialstandard
+
+go 1.23.1
+
+require (
+	github.com/goccy/go-json v0.10.5
+	github.com/gofiber/fiber/v2 v2.52.9
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.7.5
+	github.com/lib/pq v1.10.9
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sethvargo/go-envconfig v1.3.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)

backend/internal/service/server.go
@@ -1,0 +1,70 @@
+package service
+
+import (
+	"specialstandard/internal/config"
+	"specialstandard/internal/errs"
+	"specialstandard/internal/storage"
+	"specialstandard/internal/storage/postgres"
+
+	"context"
+	"net/http"
+
+	go_json "github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+type App struct {
+	Server *fiber.App
+	Repo   *storage.Repository
+}
+
+// Initialize the App union type containing a fiber app, a repository, and a climatiq client.
+func InitApp(config config.Config) *App {
+	ctx := context.Background()
+	repo := postgres.NewRepository(ctx, config.DB)
+
+	app := SetupApp(config, repo)
+
+	return &App{
+		Server: app,
+		Repo:   repo,
+	}
+}
+
+// Setup the fiber app with the specified configuration, database, and climatiq client.
+func SetupApp(config config.Config, repo *storage.Repository) *fiber.App {
+	app := fiber.New(fiber.Config{
+		JSONEncoder:  go_json.Marshal,
+		JSONDecoder:  go_json.Unmarshal,
+		ErrorHandler: errs.ErrorHandler,
+	})
+
+	app.Use(recover.New())
+	app.Use(favicon.New())
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed,
+	}))
+
+	// Use logging middleware
+	app.Use(logger.New())
+
+	// Use CORS middleware to configure CORS and handle preflight/OPTIONS requests.
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000,http://localhost:8080",
+		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS", // Using these methods.
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true, // Allow cookies
+		ExposeHeaders:    "Content-Length, X-Request-ID",
+	}))
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(http.StatusOK)
+	})
+
+	return app
+}

backend/internal/storage/postgres/storage.go
@@ -1,0 +1,43 @@
+package postgres
+
+import (
+	"specialstandard/internal/config"
+	"specialstandard/internal/storage"
+
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Establishes a sustained connection to the PostgreSQL database using pooling.
+func ConnectDatabase(ctx context.Context, config config.DB) (*pgxpool.Pool, error) {
+	dbConfig, err := pgxpool.ParseConfig(config.Connection())
+	// dbConfig, err := pgxpool.ParseConfig("postgresql://postgres:postgres@host.docker.internal:54322/postgres")
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+		return nil, err
+	}
+
+	conn, err := pgxpool.NewWithConfig(ctx, dbConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Print("Connected to database!")
+	return conn, nil
+}
+
+func NewRepository(ctx context.Context, config config.DB) *storage.Repository {
+	db, err := ConnectDatabase(ctx, config)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	return storage.NewRepository(db)
+}

GenerateNU/skillspark

backend/cmd/main.go
@@ -1,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"os"
+	"os/signal"
+	"skillspark/internal/config"
+	"skillspark/internal/service"
+	"syscall"
+
+	"github.com/joho/godotenv"
+	"github.com/sethvargo/go-envconfig"
+)
+
+func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+		return
+	}
+
+	var config config.Config
+	if err := envconfig.Process(context.Background(), &config); err != nil {
+		log.Fatalln("Error processing .env file: ", err)
+	}
+
+	app := service.InitApp(config)
+
+	// Pushing the closing of the database connection onto a
+	// stack of statements to be executed when this function returns.
+
+	// **Uncomment after repo connection is actually made**
+	// defer app.Repo.Close()
+
+	port := config.Application.Port
+	// Listen for connections with a goroutine.
+	go func() {
+		if err := app.Server.Listen(":" + port); err != nil {
+			log.Fatalf("Failed to start server: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// Wait for the termination signal:
+	<-quit
+
+	// Then shutdown server gracefully.
+	slog.Info("Shutting down server")
+	if err := app.Server.Shutdown(); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	}
+
+	slog.Info("Server shutdown")
+}

backend/go.mod
@@ -1,0 +1,34 @@
+module skillspark
+
+go 1.23.1
+
+require (
+	github.com/goccy/go-json v0.10.5
+	github.com/gofiber/fiber/v2 v2.52.9
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v5 v5.7.5
+	github.com/lib/pq v1.10.9
+	github.com/pkg/errors v0.9.1
+)
+
+require (
+	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sethvargo/go-envconfig v1.3.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.51.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
+)

backend/internal/service/server.go
@@ -1,0 +1,70 @@
+package service
+
+import (
+	"skillspark/internal/config"
+	"skillspark/internal/errs"
+	"skillspark/internal/storage"
+	"skillspark/internal/storage/postgres"
+
+	"context"
+	"net/http"
+
+	go_json "github.com/goccy/go-json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
+type App struct {
+	Server *fiber.App
+	Repo   *storage.Repository
+}
+
+// Initialize the App union type containing a fiber app, a repository, and a climatiq client.
+func InitApp(config config.Config) *App {
+	ctx := context.Background()
+	repo := postgres.NewRepository(ctx, config.DB)
+
+	app := SetupApp(config, repo)
+
+	return &App{
+		Server: app,
+		Repo:   repo,
+	}
+}
+
+// Setup the fiber app with the specified configuration, database, and climatiq client.
+func SetupApp(config config.Config, repo *storage.Repository) *fiber.App {
+	app := fiber.New(fiber.Config{
+		JSONEncoder:  go_json.Marshal,
+		JSONDecoder:  go_json.Unmarshal,
+		ErrorHandler: errs.ErrorHandler,
+	})
+
+	app.Use(recover.New())
+	app.Use(favicon.New())
+	app.Use(compress.New(compress.Config{
+		Level: compress.LevelBestSpeed,
+	}))
+
+	// Use logging middleware
+	app.Use(logger.New())
+
+	// Use CORS middleware to configure CORS and handle preflight/OPTIONS requests.
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     "http://localhost:3000,http://localhost:8080",
+		AllowMethods:     "GET,POST,PUT,PATCH,DELETE,OPTIONS", // Using these methods.
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowCredentials: true, // Allow cookies
+		ExposeHeaders:    "Content-Length, X-Request-ID",
+	}))
+
+	app.Get("/health", func(c *fiber.Ctx) error {
+		return c.SendStatus(http.StatusOK)
+	})
+
+	return app
+}

backend/internal/storage/postgres/storage.go
@@ -1,0 +1,43 @@
+package postgres
+
+import (
+	"skillspark/internal/config"
+	"skillspark/internal/storage"
+
+	"context"
+	"log"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+// Establishes a sustained connection to the PostgreSQL database using pooling.
+func ConnectDatabase(ctx context.Context, config config.DB) (*pgxpool.Pool, error) {
+	dbConfig, err := pgxpool.ParseConfig(config.Connection())
+	// dbConfig, err := pgxpool.ParseConfig("postgresql://postgres:postgres@host.docker.internal:54322/postgres")
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+		return nil, err
+	}
+
+	conn, err := pgxpool.NewWithConfig(ctx, dbConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	err = conn.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Print("Connected to database!")
+	return conn, nil
+}
+
+func NewRepository(ctx context.Context, config config.DB) *storage.Repository {
+	db, err := ConnectDatabase(ctx, config)
+	if err != nil {
+		log.Fatalf("Failed to connect to the database: %v", err)
+	}
+
+	return storage.NewRepository(db)
+}