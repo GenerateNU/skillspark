GenerateNU/specialstandard

example endpoint and supabase setup

backend/internal/service/handler/session/handler.go
@@ -1,0 +1,13 @@
+package session
+
+import "specialstandard/internal/storage"
+
+type Handler struct {
+	sessionRepository storage.SessionRepository
+}
+
+func NewHandler(sessionRepository storage.SessionRepository) *Handler {
+	return &Handler{
+		sessionRepository,
+	}
+}

backend/internal/service/server.go
@@ -3,6 +3,7 @@ package service
 import (
 	"specialstandard/internal/config"
 	"specialstandard/internal/errs"
+	"specialstandard/internal/service/handler/session"
 	"specialstandard/internal/storage"
 	"specialstandard/internal/storage/postgres"
 
@@ -64,6 +65,12 @@ func SetupApp(config config.Config, repo *storage.Repository) *fiber.App {
 
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendStatus(http.StatusOK)
+	})
+
+	// Setup routes
+	sessionHandler := session.NewHandler(repo.Session)
+	app.Route("/sessions", func(r fiber.Router) {
+		r.Get("/", sessionHandler.GetSessions)
 	})
 
 	return app

backend/internal/storage/postgres/schema/sessions.go
@@ -1,0 +1,37 @@
+package schema
+
+import (
+	"context"
+	"specialstandard/internal/models"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type SessionRepository struct {
+	db *pgxpool.Pool
+}
+
+func (r *SessionRepository) GetSessions(ctx context.Context) ([]models.Session, error) {
+	query := `
+	SELECT id, therapist_id, session_date, start_time, end_time, notes, created_at, updated_at
+	FROM sessions`
+
+	rows, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	sessions, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Session])
+	if err != nil {
+		return nil, err
+	}
+	return sessions, nil
+}
+
+func NewSessionRepository(db *pgxpool.Pool) *SessionRepository {
+	return &SessionRepository{
+		db,
+	}
+}

backend/internal/storage/storage.go
@@ -1,11 +1,20 @@
 package storage
 
 import (
+	"context"
+	"specialstandard/internal/models"
+	"specialstandard/internal/storage/postgres/schema"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type SessionRepository interface {
+	GetSessions(ctx context.Context) ([]models.Session, error)
+}
+
 type Repository struct {
-	db *pgxpool.Pool
+	db      *pgxpool.Pool
+	Session SessionRepository
 }
 
 func (r *Repository) Close() error {
@@ -19,6 +28,7 @@ func (r *Repository) GetDB() *pgxpool.Pool {
 
 func NewRepository(db *pgxpool.Pool) *Repository {
 	return &Repository{
-		db: db,
+		db:      db,
+		Session: schema.NewSessionRepository(db),
 	}
 }

GenerateNU/skillspark

backend/internal/service/handler/session/handler.go
@@ -1,0 +1,13 @@
+package session
+
+import "skillspark/internal/storage"
+
+type Handler struct {
+	sessionRepository storage.SessionRepository
+}
+
+func NewHandler(sessionRepository storage.SessionRepository) *Handler {
+	return &Handler{
+		sessionRepository,
+	}
+}

backend/internal/service/server.go
@@ -3,6 +3,7 @@ package service
 import (
 	"skillspark/internal/config"
 	"skillspark/internal/errs"
+	"skillspark/internal/service/handler/session"
 	"skillspark/internal/storage"
 	"skillspark/internal/storage/postgres"
 
@@ -64,6 +65,12 @@ func SetupApp(config config.Config, repo *storage.Repository) *fiber.App {
 
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.SendStatus(http.StatusOK)
+	})
+
+	// Setup routes
+	sessionHandler := session.NewHandler(repo.Session)
+	app.Route("/sessions", func(r fiber.Router) {
+		r.Get("/", sessionHandler.GetSessions)
 	})
 
 	return app

backend/internal/storage/postgres/schema/sessions.go
@@ -1,0 +1,37 @@
+package schema
+
+import (
+	"context"
+	"skillspark/internal/models"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type SessionRepository struct {
+	db *pgxpool.Pool
+}
+
+func (r *SessionRepository) GetSessions(ctx context.Context) ([]models.Session, error) {
+	query := `
+	SELECT id, therapist_id, session_date, start_time, end_time, notes, created_at, updated_at
+	FROM sessions`
+
+	rows, err := r.db.Query(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	sessions, err := pgx.CollectRows(rows, pgx.RowToStructByName[models.Session])
+	if err != nil {
+		return nil, err
+	}
+	return sessions, nil
+}
+
+func NewSessionRepository(db *pgxpool.Pool) *SessionRepository {
+	return &SessionRepository{
+		db,
+	}
+}

backend/internal/storage/storage.go
@@ -1,11 +1,20 @@
 package storage
 
 import (
+	"context"
+	"skillspark/internal/models"
+	"skillspark/internal/storage/postgres/schema"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type SessionRepository interface {
+	GetSessions(ctx context.Context) ([]models.Session, error)
+}
+
 type Repository struct {
-	db *pgxpool.Pool
+	db      *pgxpool.Pool
+	Session SessionRepository
 }
 
 func (r *Repository) Close() error {
@@ -19,6 +28,7 @@ func (r *Repository) GetDB() *pgxpool.Pool {
 
 func NewRepository(db *pgxpool.Pool) *Repository {
 	return &Repository{
-		db: db,
+		db:      db,
+		Session: schema.NewSessionRepository(db),
 	}
 }