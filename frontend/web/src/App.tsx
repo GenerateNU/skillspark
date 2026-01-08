94f3e2f010abd243bc81147598e9d016ce6ee64e
Josh Torre <torre.jo@northeastern.edu>
2026-01-08 16:02:03 -0500

    Web init

---
+++ frontend/web/src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+        <div className="w-screen w-screen p-8 flex justify-center items-center">
+          <h1 className="text-4xl font-bold">Welcome to SkillSpark!</h1>
+      </div>
+  );
+}
+
+export default App;
