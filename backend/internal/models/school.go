e77fc8a17f7bf7071fb80f94aff4da2f04441906
Josh Torre <torre.jo@northeastern.edu>
2026-01-14 11:56:17 -0500

    Fix school model def

---
+++ backend/internal/models/school.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type School struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	Name      string    `json:"name" db:"name"`
+	Location  Location  `json:"location" db:"location"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type GetAllSchoolsOutput struct {
+	Body []School `json:"body" doc:"List of all schools in the database"`
+}
