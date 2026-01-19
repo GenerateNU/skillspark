eae070d654cec245528432a50eab8a5d96be990f
vdema <demaisip.v@northeastern.edu>
2026-01-19 13:08:40 -0500

    Added Event Mock Repository

---
+++ backend/internal/storage/repo-mocks/eventMock.go
@@ -0,0 +1,44 @@
+package repomocks
+
+import (
+	"context"
+	"skillspark/internal/errs"
+	"skillspark/internal/models"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockEventRepository struct {
+	mock.Mock
+}
+
+func (m *MockEventRepository) CreateEvent(ctx context.Context, input *models.CreateEventInput) (*models.Event, *errs.HTTPError) {
+	args := m.Called(ctx, input)
+	if args.Get(0) == nil {
+		if args.Get(1) == nil {
+			return nil, nil
+		}
+		return nil, args.Get(1).(*errs.HTTPError)
+	}
+	return args.Get(0).(*models.Event), nil
+}
+
+func (m *MockEventRepository) PatchEvent(ctx context.Context, input *models.PatchEventInput) (*models.Event, *errs.HTTPError) {
+	args := m.Called(ctx, input)
+	if args.Get(0) == nil {
+		if args.Get(1) == nil {
+			return nil, nil
+		}
+		return nil, args.Get(1).(*errs.HTTPError)
+	}
+	return args.Get(0).(*models.Event), nil
+}
+
+func (m *MockEventRepository) DeleteEvent(ctx context.Context, id uuid.UUID) (*struct{}, error) {
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*struct{}), args.Error(1)
+}
