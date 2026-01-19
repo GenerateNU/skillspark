ada81275af62eaff1121f500c8e2abcb833c40ca
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-01-19 15:05:34 -0500

    one endpoint :3

--- backend/internal/storage/postgres/schema/child/get_by_id.go
+++ backend/internal/storage/postgres/schema/child/get_by_id.go
@@ -2,12 +2,35 @@ package child
 
 import (
 	"context"
+	"errors"
 	"skillspark/internal/errs"
 	"skillspark/internal/models"
+	"skillspark/internal/storage/postgres/schema"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 )
 
-func (c *ChildRepository) GetChildByID(ctx context.Context, childID uuid.UUID) (*models.Child, *errs.HTTPError) {
-	return nil, nil
+func (r *ChildRepository) GetChildByID(ctx context.Context, childID uuid.UUID) (*models.Child, *errs.HTTPError) {
+	query, err := schema.ReadSQLBaseScript("child/sql/get_by_id.sql")
+	if err != nil {
+		errr := errs.InternalServerError("Failed to read base query: ", err.Error())
+		return nil, &errr
+	}
+
+	row := r.db.QueryRow(ctx, query, childID)
+	var child models.Child
+	err = row.Scan(&child.ID, &child.SchoolID, &child.SchoolName, &child.BirthMonth, &child.BirthYear, &child.Interests, &child.GuardianID, &child.CreatedAt, &child.UpdatedAt)
+
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			err := errs.NotFound("Child", "id", childID)
+			return nil, &err
+		}
+		err := errs.InternalServerError("Failed to fetch child by id: ", err.Error())
+		return nil, &err
+	}
+
+	return &child, nil
+
 }
