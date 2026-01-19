478f61b5f246b48cc92358417a98f09932c48d52
Luisa <103768169+luisa-li@users.noreply.github.com>
2026-01-19 15:11:30 -0500

    create endpoint ;3

--- backend/internal/storage/postgres/schema/child/create.go
+++ backend/internal/storage/postgres/schema/child/create.go
@@ -4,8 +4,35 @@ import (
 	"context"
 	"skillspark/internal/errs"
 	"skillspark/internal/models"
+	"skillspark/internal/storage/postgres/schema"
 )
 
-func (c *ChildRepository) CreateChild(ctx context.Context, child *models.CreateChildInput) (*models.Child, *errs.HTTPError) {
-	return nil, nil
+func (r *ChildRepository) CreateChild(ctx context.Context, child *models.CreateChildInput) (*models.Child, *errs.HTTPError) {
+
+	query, err := schema.ReadSQLBaseScript("child/sql/get_by_id.sql")
+	if err != nil {
+		errr := errs.InternalServerError("Failed to read base query: ", err.Error())
+		return nil, &errr
+	}
+
+	row := r.db.QueryRow(ctx, query,
+		child.Body.Name,
+		child.Body.SchoolID,
+		child.Body.BirthMonth,
+		child.Body.BirthYear,
+		child.Body.Interests,
+		child.Body.GuardianID,
+	)
+
+	var createdChild models.Child
+
+	err = row.Scan(&createdChild.ID, &createdChild.SchoolID, &createdChild.SchoolName, &createdChild.BirthMonth, &createdChild.BirthYear, &createdChild.Interests, &createdChild.GuardianID, &createdChild.CreatedAt, &createdChild.UpdatedAt)
+
+	if err != nil {
+		err := errs.InternalServerError("Failed to create child: ", err.Error())
+		return nil, &err
+	}
+
+	return &createdChild, nil
+
 }
