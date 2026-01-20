9c13c2264544aef7b9550b0dea1a29bb8e3f1a76
Josh Torre <torre.jo@northeastern.edu>
2026-01-20 03:27:58 -0500

    Extract the config logic to a new function

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -6,44 +6,19 @@ import (
 	"log/slog"
 	"os"
 	"os/signal"
-	"skillspark/internal/config"
 	"skillspark/internal/service"
 	"syscall"
 	"time"
-
-	"github.com/joho/godotenv"
-	"github.com/sethvargo/go-envconfig"
 )
 
 func main() {
-	//load the top level env file to determine the environment
-	environment := os.Getenv("ENVIRONMENT")
-
-	var cfg config.Config
-	switch environment {
-	case "production":
-		// Load configuration from environment variables for production
-		err := envconfig.Process(context.Background(), &cfg)
-		if err != nil {
-			log.Fatalln("Error processing environment variables: ", err)
-		}
-	case "development":
-		// Load configuration from environment variables for development
-		err := godotenv.Overload("../.local.env")
-		if err != nil {
-			log.Fatalln("Error loading .local.env file: ", err)
-		}
-		err = envconfig.Process(context.Background(), &cfg)
-		if err != nil {
-			log.Fatalln("Error processing environment variables: ", err)
-		}
-	default:
-		log.Fatalln("Invalid environment name: ", environment, "The environment name must be one of either production or development")
-		return
+	cfg, err := LoadConfig()
+	if err != nil {
+		log.Fatalf("Failed to load config: %v", err)
 	}
 
 	// Initialize application with config
-	app := service.InitApp(cfg)
+	app := service.InitApp(*cfg)
 
 	// Close database connection when main exits
 	defer func() {
