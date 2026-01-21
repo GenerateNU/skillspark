f4c8f005697e36def2a4cab2d73220c5aa463651
adescoteaux1 <ally.descoteaux.0@gmail.com>
2026-01-21 08:20:07 -0500

    get all schools fix

--- backend/internal/service/handler/school/getAllSchools.go
+++ backend/internal/service/handler/school/getAllSchools.go
@@ -2,11 +2,12 @@ package school
 
 import (
 	"context"
+	"skillspark/internal/errs"
 	"skillspark/internal/models"
 	"skillspark/internal/utils"
 )
 
-func (h *Handler) GetAllSchools(ctx context.Context, pagination utils.Pagination) ([]models.School, error) {
+func (h *Handler) GetAllSchools(ctx context.Context, pagination utils.Pagination) ([]models.School, *errs.HTTPError) {
 	schools, err := h.SchoolRepository.GetAllSchools(ctx, pagination)
 	if err != nil {
 		return nil, err.(*errs.HTTPError)
