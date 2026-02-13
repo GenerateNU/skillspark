f53f1e4c8eea39c412bd318f9c12172cab7114db
gregorysa <gregory.sa@northeastern.edu>
2026-02-12 23:38:06 -0500

    dropped auth_id column and added it to change type to uuid

--- backend/internal/supabase/migrations/20260209065709_alter_user_auth_not_null.sql
+++ backend/internal/supabase/migrations/20260209065709_alter_user_auth_not_null.sql
@@ -1,7 +1,10 @@
+-- drop column and re-add with auth_id as UUID
+ALTER TABLE "user"
+DROP COLUMN IF EXISTS auth_id;
+
+ALTER TABLE "user"
+ADD COLUMN auth_id UUID UNIQUE;
+
 -- make auth_id a required field
 ALTER TABLE "user"
 ALTER COLUMN auth_id SET NOT NULL;
-
--- make auth_id type UUID
-ALTER TABLE "user"
-ALTER COLUMN auth_id SET DATA TYPE UUID;
