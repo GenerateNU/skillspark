c2f970c553ca2fbd6aa184e2917f1e0f44ff557c
vdema <demaisip.v@northeastern.edu>
2026-02-17 18:17:04 -0500

    Exported GET functions from users and child

--- frontend/packages/api-client/src/index.ts
+++ frontend/packages/api-client/src/index.ts
@@ -2,3 +2,5 @@
 export * from './generated/event-occurrences/event-occurrences';
 export * from './generated/events/events';
 export * from './generated/skillSparkAPI.schemas';
+export * from './generated/users/users';
+export * from './generated/child/child';
