bdc5341174684d38f57ee2793145e7d63511b87c
gregorysa <gregory.sa@northeastern.edu>
2026-02-16 20:53:17 -0500

    login + signup web interfaces

--- frontend/apps/web/src/signup/signup.tsx
+++ frontend/apps/web/src/signup/signup.tsx
@@ -1,18 +1,22 @@
-export default function Login() {
+function SignUp() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold pb-16">Log In</h1>
+      <h1 className="text-4xl font-bold pb-16">Sign Up</h1>
       <div className="flex flex-col items-center justify-center gap-4 w-full max-w-md">
+        <input type="text" placeholder="Name" />
         <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
+        <input type="text" placeholder="Language Preference" />
         <button
           type="submit"
           className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600"
         >
-          Log in
+          Sign up
         </button>
       </div>
     </div>
   );
 }
 
+export default SignUp;
