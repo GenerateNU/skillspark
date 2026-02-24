623390a3e9c67dbfb89411aa7e4a0d9ce4e382f7
Alen Ganopolsky <alenganopolsky@gmail.com>
2026-02-23 20:47:18 -0500

    fixed update tests

--- backend/internal/storage/postgres/schema/event/update.go
+++ backend/internal/storage/postgres/schema/event/update.go
@@ -17,7 +17,8 @@ func (r *EventRepository) UpdateEvent(ctx context.Context, input *models.UpdateE
 	row := r.db.QueryRow(ctx, query, input.ID, input.Body.Title_EN, input.Body.Title_TH, input.Body.Description_EN, input.Body.Description_TH, input.Body.OrganizationID, input.Body.AgeRangeMin, input.Body.AgeRangeMax, input.Body.Category, HeaderImageS3Key)
 
 	var event models.Event
-	var titleEN, titleTH, descEN, descTH string
+	var titleEN, descEN string
+	var titleTH, descTH *string
 
 	err = row.Scan(&event.ID, &titleEN, &titleTH, &descEN, &descTH, &event.OrganizationID, &event.AgeRangeMin, &event.AgeRangeMax, &event.Category, &event.HeaderImageS3Key, &event.CreatedAt, &event.UpdatedAt)
 	if err != nil {
@@ -25,9 +26,9 @@ func (r *EventRepository) UpdateEvent(ctx context.Context, input *models.UpdateE
 		return nil, &err
 	}
 
-	if input.AcceptLanguage == "th" {
-		event.Title = titleTH
-		event.Description = descTH
+	if input.AcceptLanguage == "th" && titleTH != nil {
+		event.Title = *titleTH
+		event.Description = *descTH
 	} else {
 		event.Title = titleEN
 		event.Description = descEN
