aff6cc8d8bf2093ea081c84b1baf213b53ffa4b8
bpalazzi512 <60822022+bpalazzi512@users.noreply.github.com>
2026-03-24 23:56:31 -0400

    safe area

--- frontend/apps/mobile/app/(app)/(tabs)/activity.tsx
+++ frontend/apps/mobile/app/(app)/(tabs)/activity.tsx
@@ -1,9 +1,11 @@
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function ActivityScreen() {
+  const insets = useSafeAreaInsets();
   return (
-    <ThemedView>
+    <ThemedView style={{ paddingTop: insets.top }}>
       <ThemedText>hellooooooo</ThemedText>
     </ThemedView>
   );
