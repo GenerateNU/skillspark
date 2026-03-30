1a932717de20ad46895a441490c4432add6d2131
Karim Camara <karimjcamara@gmail.com>
2026-03-29 23:23:16 -0400

    fixed profile

--- frontend/apps/web/src/vite-env.d.ts
+++ frontend/apps/web/src/vite-env.d.ts
@@ -1,3 +1,11 @@
 /// <reference types="vite/client" />
 
 declare module "@tailwindcss/vite";
+
+interface ImportMetaEnv {
+  readonly VITE_ADMIN_NAME: string;
+  readonly VITE_ADMIN_EMAIL: string;
+  readonly VITE_ADMIN_USERNAME: string;
+  readonly VITE_ADMIN_LANG_PREFERENCE: string;
+  readonly VITE_ADMIN_ROLE: string;
+}
