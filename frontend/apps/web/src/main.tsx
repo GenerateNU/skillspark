eb94b8ef3d38afb1a875051aa840dfb48ae30d3d
gregorysa <gregory.sa@northeastern.edu>
2026-04-02 15:55:13 -0400

    idek

--- frontend/apps/web/src/main.tsx
+++ frontend/apps/web/src/main.tsx
@@ -4,14 +4,18 @@ import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./login/login.tsx";
 import { AuthProvider } from "./contexts/auth-context.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./constants/query-client.ts";
 
 createRoot(document.getElementById("root")!).render(
-	<AuthProvider>
+	<QueryClientProvider client={queryClient}>
 		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<App />} />
-				<Route path="/login" element={<Login />} />
-			</Routes>
+			<AuthProvider>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</AuthProvider>
 		</BrowserRouter>
-	</AuthProvider>,
+	</QueryClientProvider>,
 );
