4f9112edaeb88139a2c672c4913171268690c86c
Alen Ganopolsky <159747193+AlenGanopolsky@users.noreply.github.com>
2026-04-04 17:48:50 -0400

    Feat/tinder swipe (#175)
    * weird dockerfile stuff
    
    * fixed issue..?
    
    * added tinder swipe mechanism to 'discover new' feature
    
    * misc
    
    * reverted docker-compose file
    
    * Uncomment build command in Dockerfile
    
    ---------
    
    Co-authored-by: Ally Descoteaux <128718372+adescoteaux1@users.noreply.github.com>

---
+++ frontend/apps/mobile/components/home/CarouselCard.tsx
@@ -0,0 +1,31 @@
+import Carousel from 'react-native-reanimated-carousel';
+import { View } from 'react-native';
+import { AppColors } from "@/constants/theme";
+import { type EventOccurrence } from '@skillspark/api-client';
+import { DiscoverBanner } from './DiscoverBanner';
+
+export default function CarouselCard({ events, width, height}: { events: EventOccurrence[], width: number, height: number }) {
+    
+  return (
+    <Carousel
+      width={width / 1.1}
+      height={height / 4}
+      data={events}
+      loop
+      autoPlay
+      autoPlayInterval={3000}
+      mode="vertical-stack"
+      modeConfig={{
+        snapDirection: 'left',
+        stackInterval: 0,
+      }}
+
+      renderItem={({ item }) => (
+        <View className="borderRadius: 24, overflow: 'hidden', flex: 1">
+          <DiscoverBanner event={item} />
+        </View>
+      )}
+      style={{ alignSelf: 'center' }}
+    />
+  );
+}
