a61e2f718d1ba9fdb2615891b65eb7856632cb46
vdema <demaisip.v@northeastern.edu>
2026-04-04 12:40:00 -0400

    Updated imports to index.ts

--- frontend/packages/api-client/src/index.ts
+++ frontend/packages/api-client/src/index.ts
@@ -10,3 +10,5 @@ export * from "./generated/auth/auth";
 export * from "./generated/saved/saved";
 export * from "./generated/registrations/registrations";
 export * from './generated/emergency-contacts/emergency-contacts';
+export * from './generated/organizations/organizations';
+export * from './generated/locations/locations';
