4a29f8668f1210501d9606baa8b1cc4a20463427
Karim Camara <karimjcamara@gmail.com>
2026-04-05 16:22:21 -0400

    quick fix

--- frontend/apps/mobile/app/(app)/(tabs)/payment.tsx
+++ frontend/apps/mobile/app/(app)/(tabs)/payment.tsx
@@ -124,8 +124,6 @@ export default function PaymentScreen() {
               key={i}
             />
           ))
-        ) : error ? (
-          <></>
         ) : (
           <ThemedText
             className="text-base font-nunito mb-8"
