257f11c1f04c855d0fbc2a8c53c1972da5cbbaeb
adescoteaux1 <ally.descoteaux.0@gmail.com>
2026-04-06 15:52:10 -0400

    fixes

--- frontend/packages/api-client/src/index.ts
+++ frontend/packages/api-client/src/index.ts
@@ -2,6 +2,14 @@ export { setCurrentLanguage } from "./apiClient";
 // Re-export everything from generated
 export * from "./generated/event-occurrences/event-occurrences";
 export * from "./generated/events/events";
+export type {
+  HTTPStatusCode1xx,
+  HTTPStatusCode2xx,
+  HTTPStatusCode3xx,
+  HTTPStatusCode4xx,
+  HTTPStatusCode5xx,
+  HTTPStatusCodes,
+} from "./generated/event-occurrences/event-occurrences";
 export * from "./generated/skillSparkAPI.schemas";
 export * from "./generated/child/child";
 export * from "./generated/guardians/guardians";
