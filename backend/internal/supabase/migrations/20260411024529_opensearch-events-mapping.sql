ee77d8d51263f4bf46209776ab4551b6a7ee4971
Alen Ganopolsky <alenganopolsky@gmail.com>
2026-04-11 02:56:43 -0400

    fixed test issue related to pg_net -> in migration file

--- backend/internal/supabase/migrations/20260411024529_opensearch-events-mapping.sql
+++ backend/internal/supabase/migrations/20260411024529_opensearch-events-mapping.sql
@@ -1,18 +1,22 @@
 CREATE OR REPLACE FUNCTION notify_opensearch()
 RETURNS TRIGGER AS $$
 BEGIN
-  PERFORM net.http_post(
-    url     := 'http://supabase_edge_runtime_skillspark:8081/opensearch-access',
-    headers := jsonb_build_object(
-      'Content-Type',  'application/json',
-      'Authorization', 'Bearer sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH'
-    ),
-    body    := jsonb_build_object(
-      'type',       TG_OP,
-      'record',     row_to_json(NEW),
-      'old_record', row_to_json(OLD)
-    )
-  );
+  BEGIN
+    PERFORM net.http_post(
+      url     := 'http://supabase_edge_runtime_skillspark:8081/opensearch-access',
+      headers := jsonb_build_object(
+        'Content-Type',  'application/json',
+        'Authorization', 'Bearer sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH'
+      ),
+      body    := jsonb_build_object(
+        'type',       TG_OP,
+        'record',     row_to_json(NEW),
+        'old_record', row_to_json(OLD)
+      )
+    );
+  EXCEPTION WHEN OTHERS THEN
+    NULL;
+  END;
   RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
