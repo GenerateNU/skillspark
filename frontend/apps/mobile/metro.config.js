e8f8f536700a1015d9277650a1aa0ecaff56b37d
gregorysa <gregory.sa@northeastern.edu>
2026-04-17 05:44:50 -0400

    Revert "fixing after merge"
    This reverts commit 7ab2019163a37ec53655daf13a5afaf6b0795fa4.

--- frontend/apps/mobile/metro.config.js
+++ frontend/apps/mobile/metro.config.js
@@ -35,8 +35,6 @@ const nativeWindConfig = withNativeWind(config, { input: "./global.css" });
 const DEDUPLICATED_PACKAGES = [
 	"react",
 	"react-native",
-	"expo-router",
-	"expo-clipboard",
 	"react-native-css-interop",
 	"react-native-safe-area-context",
 	"react-native-screens",
